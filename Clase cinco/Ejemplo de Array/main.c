5ee6289cfb6e2d2fc0e84f7e1b3623c3e7919d61
alumno <alumno>
2019-09-05 11:59:04 -0300

    Clase 5 parte dos

--- Clase cinco/Ejemplo de Array/main.c
+++ Clase cinco/Ejemplo de Array/main.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CANTIDAD 3
+#define CANTIDAD 6
 
 void mostrarArray(int miArray[],int cantidad);
 int dameMax(int miArray[],int cantidad);
 int dameMin(int miArray[],int cantidad);
 void inicializarArray(int miArray[],int cantidad);
 void cargaSecuencialArray(int miArray[],int cantidad,char mensaje[]);
+float darmePromedio (int miArray[],int cantidad);
+void ordenarBurbujeo (int miArray[],int cantidad);
+
 
 int main()
 {
-    int edad[10];
+    int edad[6];
     inicializarArray(edad,CANTIDAD);
     cargaSecuencialArray(edad,CANTIDAD,"la edad");
+    printf("Antes de ordenar:\n");
+    mostrarArray(edad,CANTIDAD);
+    ordenarBurbujeo (edad,CANTIDAD);
+    printf("Despues de ordenar:\n");//Nunca te olvides los if (de menor a mayor i mayor a j
     mostrarArray(edad,CANTIDAD);
     printf("El max es: %d\n",dameMax(edad,CANTIDAD));
     printf("El min es: %d\n",dameMin(edad,CANTIDAD));
+    printf("El promedio de edades es de %.2f\n",darmePromedio(edad,CANTIDAD));
 }
 
 void mostrarArray(int miArray[],int cantidad)
@@ -74,5 +82,36 @@ void cargaSecuencialArray(int miArray[],int cantidad,char mensaje[])
     }
 }
 
+float darmePromedio (int miArray[],int cantidad)
+{
+    int i;
+    float acumulador=0;
 
+    for (i=0;i<cantidad;i++)
+    {
+        acumulador = acumulador + miArray[i];
+    }
+
+    return acumulador/cantidad;
+}
+
+void ordenarBurbujeo (int miArray[],int cantidad)
+{
+    int i;
+    int j;
+    int aux;
+
+    for (i=0;i<cantidad-1;i++)
+    {
+        for (j=i+1;j<cantidad;j++)
+        {
+            if (miArray [i]>miArray[j])
+            {
+            aux = miArray[i];
+            miArray[i] = miArray[j];
+            miArray[j] = aux;
+            }
+        }
+    }
+}
 
