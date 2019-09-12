a62478289baf1f72a4212e2cfad09daa200fa989
alumno <alumno>
2019-09-12 10:50:57 -0300

    Actualizo

--- Clase siete/Arrays/main.c
+++ Clase siete/Arrays/main.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #define TAM 8
+#define TAMALUMNOS 4
 
 //CARGA EL ARRAY CON DATOS
 void cargarConDatosEnteros(int [],int);
 //CARGA ARRAY CON NOMBRES
 void cargarNombres(char [][50],int);
 
+
+
 int main()
 {
     int edades [TAM];
+    int notas [TAMALUMNOS];
     char nombres [TAM] [50];
     int i;
+    int max,iMax;
 
    /* for (i=0;i<3;i++)
     {
@@ -24,20 +29,37 @@ int main()
     }
 
     */
-    cargarConDatosEnteros(edades,TAM);
+    /*cargarConDatosEnteros(edades,TAM);
     cargarNombres(nombres,TAM);
+    */
 
+    cargarConDatosEnteros(notas,TAMALUMNOS);
+    cargarNombres(nombres,TAMALUMNOS);
+
+    max=notas[0];
+
+    for (i=0;i<TAMALUMNOS;i++)
+    {
+        if (max<notas[i])
+        {
+            iMax=i;
+        }
+    }
+    printf("\nLa nota mas alta es la de: %s", nombres[iMax]);
+
+    /*
     for (i=0;i<TAM;i++)
     {
         printf("\nSu edad es: %d anios y su nombre es %s ", edades[i], nombres[i]);
     }
+    */
 
     return 0;
 }
 
 void cargarConDatosEnteros(int miArray [],int cantidad)
 {
-    int aux[]={55,11,22,33,18,15,10,66};
+    int aux[]={4,10,8,2};
     int i;
     for (i=0;i<cantidad;i++)
     {
@@ -47,7 +69,7 @@ void cargarConDatosEnteros(int miArray [],int cantidad)
 
 void cargarNombres(char miArray [][50],int cantidad)
 {
-    char aux[][50]={"Jorge","Ricardo","Lionel","Mauro","Manuel","Felipe","Damian","Jose"};
+    char aux[][50]={"Alfredo","Rogelio","Julieta","Pablo"};
     int i;
     for (i=0;i<cantidad;i++)
     {
