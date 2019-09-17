29aae3741fabaa28da685099ab66e2be37ed89a9
marianoefernandez <mezequielfernandez4@hotmail.com>
2019-09-16 22:25:05 -0300

    Actualizo

--- Biblioteca UTN/utn biblioteca/utn.h
+++ Biblioteca UTN/utn biblioteca/utn.h
@@ -1,8 +1,67 @@
-//#include
-//#define
+//#include (Poner todos)
+//#define  TAM (NRO)
+
+//CLASE TRES
+//SUMA Y PEDIDA DE DATOS
+
+float sumar (int ,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float restar (int ,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float multiplicar (int ,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float dividir (int ,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrar(float);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int pedirDato();
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+//CLASE CUATRO
+//FUNCIONES PARA PEDIR DATOS
 
 int getInt (char[]);
-
 /** \brief Función que pide dato al usuario y retorna lo que el usuario puso (int)
  *
  * \param 1 es el entero que guarda el valor que el usuario pondra y sera el que retorne
@@ -12,7 +71,6 @@ int getInt (char[]);
  */
 
 char getChar (char[]);
-
 /** \brief Función que pide dato al usuario y retorna lo que el usuario puso (char)
  *
  * \param 1 es el char que guarda el valor que el usuario pondra y sera el que retorne
@@ -27,6 +85,281 @@ float getFloat (char[]);
  * \param 1 es el flotante que guarda el valor que el usuario pondra y sera el que retorne
  * \param 2 el mensaje de que es lo que pide
  * \return retorna el numero que el usuario ingreso
+ *
+ */
+
+
+//CLASE CINCO
+//BURBUJEO Y PROMEDIO
+
+void mostrarArray(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int dameMax(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int dameMin(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void inicializarArray(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void cargaSecuencialArray(int [],int,char []);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float darmePromedio (int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void ordenarBurbujeo (int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void burbujeoMejorado (int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void insersion (int [],int);
+
+//CLASE SEIS
+//OPERACIONES SOBRE ARRAYS
+
+void cargarVector(int [], int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarVector(int [], int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarNegativos(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarPromedio(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int sumarPositivos(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int contarPositivos(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float calcularPromedio(int,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+//CLASE SIETE
+//MÁS OPRACIONES CON ARRAYS
+
+void cargarVector(int [], int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarVector(int [], int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarNegativos(int [],int tam);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarPromedio(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int sumarPositivos(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int contarPositivos(int [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+float calcularPromedio(int,int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+//CLASE OCHO ESTRUCTURAS
+
+void mostrarAlumno (eAlumno);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+eAlumno pedirAlumno();
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+int compararAlumnoPorLegajo(eAlumno, eAlumno);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void cargarAlumnos(eAlumno [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void mostrarListadoAlumnos (eAlumno [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void ordenarAlumnosPorNombres(eAlumno [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
+ *
+ */
+
+void hardcodearAlumnos(eAlumno [],int);
+/** \brief
+ *
+ * \param
+ * \param
+ * \return
  *
  */
 
