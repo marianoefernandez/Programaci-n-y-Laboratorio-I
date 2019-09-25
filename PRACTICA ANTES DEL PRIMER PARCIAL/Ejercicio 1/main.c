0de70b051ec3611c8fda902a24f2629e53b91d35
marianoefernandez <mezequielfernandez4@hotmail.com>
2019-09-25 00:15:38 -0300

    Actualizo

---
+++ PRACTICA ANTES DEL PRIMER PARCIAL/Ejercicio 1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Escribir un programa que realice las siguientes acciones:
+
+-Solicite al usuario 10 números.
+-Calcule cuántos son negativos y cuantos son positivos.
+-Muestre por pantalla la cantidad de negativos y la cantidad de positivos.
+
+*/
+
+int main()
+{
+    return 0;
+}
+
+//1-FUNCION GET INT
+//2-FUNCION PARA PEDIR VARIOS NUMEROS
+//3-CALCULAR NEGATIVOS
+//4-CALCULAR POSITIVOS
+//5-MOSTRAR CANTIDAD POS Y NEG
